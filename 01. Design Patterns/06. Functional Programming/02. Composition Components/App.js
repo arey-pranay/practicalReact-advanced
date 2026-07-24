421e76a892e2d1bcab8b35165daccbcf99e307e9
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-07-24 18:32:28 +0530

    Create App.js

---
+++ 01. Design Patterns/06. Functional Programming/02. Composition Components/App.js
@@ -0,0 +1,12 @@
+import { SmallButton, SmallRedButton } from "./components/composition";
+
+function App() {
+  return (
+    <>
+      <SmallButton text={"I am small!"}/>
+      <SmallRedButton text={"I am small and Red"}/>
+    </>
+  );
+}
+
+export default App;
