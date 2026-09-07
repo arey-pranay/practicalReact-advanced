ae1480ddd659c18b745d21b4cbf0bbb011d7fc0d
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-09-07 17:16:49 +0530

    Implement useDeferredValue in App component

---
+++ 02. Advanced Concepts and Hooks/useDeferredValue()/App.js
@@ -0,0 +1,17 @@
+import { useState, useDeferredValue } from "react";
+import { HeavyComponent } from "./components/heavy-component";
+
+function App() {
+    const [keyword, setKeyword] = useState("");
+    const deferredKeyword = useDeferredValue(keyword);
+    console.log("keyword", keyword);
+    console.log("deferredKeyword", deferredKeyword);
+    return (
+        <>
+            <input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+            <HeavyComponent keyword={deferredKeyword} />
+        </>
+    );
+}
+
+export default App;
